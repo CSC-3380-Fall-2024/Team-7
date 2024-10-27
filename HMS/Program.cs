621f701be26e86036ffd1474adc06a1e4769290f
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2024-10-26 21:12:19 -0500

    made statics pages for about and contact and adjusted nav bar

--- HMS/Program.cs
+++ HMS/Program.cs
@@ -2,7 +2,7 @@ using HMS.Components;
 
 
 
-string credentialPath = @"C:\Users\maure\projects\3380\hotelmanagementsystem-3f342-firebase-adminsdk-njalz-ea30d4f99f.json";
+string credentialPath = @"..\..\hotelmanagementsystem-3f342-firebase-adminsdk-njalz-ea30d4f99f.json";
 Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialPath);
 var builder = WebApplication.CreateBuilder(args);
 
