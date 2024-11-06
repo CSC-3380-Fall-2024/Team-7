d6ec07b6a3e037d9dad8b17a2f86bdb3fb261b1a
Cleveland Martin IV <tsarycleveland@gmail.com>
2024-11-05 18:35:56 -0600

    Created List function-Can be tested by clicking button on listing page to add listings

--- HMS/Components/Pages/razorComponents/ListingsBase.cs
+++ HMS/Components/Pages/razorComponents/ListingsBase.cs
@@ -1,5 +1,6 @@
 ﻿using HMS.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,121 +11,112 @@ namespace HMS.Components.Pages.razorComponents
 {
     public class ListingsBase : ComponentBase
     {
-        private int listingCount {  get; set; }
+        private int listingCount = 0;
         public IEnumerable<Listing>? Listings { get; set; }
-        public Listing[] Listings_Back = new Listing[5];
+		public List<Listing> Listings_DB = new List<Listing>();
+		public Listing[] listHolder = Array.Empty<Listing>();
         protected override Task OnInitializedAsync()
         {
-			AddItem();
-            //LoadItems();
+            LoadItems();
             return base.OnInitializedAsync();
         }
 
         private void LoadItems()
         {
-            Listing l1 = new Listing
-            {
-                Title = "Test1 " + listingCount.ToString(),
-                Description = "Description " + listingCount.ToString(),
-                BedCount = listingCount,
-                BathCount = listingCount,
-                Price = listingCount,
-                Date = 110224,
-                Picture = "image"
-            };
-
-			Listing l2 = new Listing
-			{
-				Title = "Test2 " + listingCount.ToString(),
-				Description = "Description " + listingCount.ToString(),
-				BedCount = listingCount,
-				BathCount = listingCount,
-				Price = listingCount,
-				Date = 110224,
-				Picture = "image"
-			};
-
-			Listing l3 = new Listing
-			{
-				Title = "Test3 " + listingCount.ToString(),
-				Description = "Description " + listingCount.ToString(),
-				BedCount = listingCount,
-				BathCount = listingCount,
-				Price = listingCount,
-				Date = 110224,
-				Picture = "image"
-			};
-
-			Listing l4 = new Listing
-			{
-				Title = "Test4 " + listingCount.ToString(),
-				Description = "Description " + listingCount.ToString(),
-				BedCount = listingCount,
-				BathCount = listingCount,
-				Price = listingCount,
-				Date = 110224,
-				Picture = "image"
-			};
-
-			Listing l5 = new Listing
-			{
-				Title = "Test5 " + listingCount.ToString(),
-				Description = "Description " + listingCount.ToString(),
-				BedCount = listingCount,
-				BathCount = listingCount,
-				Price = listingCount,
-				Date = 110224,
-				Picture = "image"
-			};
-
-			Listing l6 = new Listing
-			{
-				Title = "Test6 " + listingCount.ToString(),
-				Description = "Description " + listingCount.ToString(),
-				BedCount = listingCount,
-				BathCount = listingCount,
-				Price = listingCount,
-				Date = 110224,
-				Picture = "image"
-			};
-
-
-			//Listings = new List<Listing>().Append(l1);
-			Listings = new List<Listing> { l1, l2, l3, l4, l5, l6, };
+			Listings = Listings_DB;
 		}
 
+		//Add items to Listings_DB
         public void AddItem()
         {
-
-			Listings = new List<Listing>().Append(new Listing
-            {
-				Title = "Test " + listingCount.ToString(),
+			Listings_DB.Add(new Listing
+			{
+				Title = "Test Listing " + listingCount.ToString(),
 				Description = "Description " + listingCount.ToString(),
 				BedCount = listingCount,
 				BathCount = listingCount,
 				Price = listingCount,
 				Date = 110224,
-				Picture = "image"
+				Picture = "image",
+				ID = "89" + listingCount.ToString()
 			});
 
-
-			//Console.WriteLine("ok");
-			//Listings = Listings.Append(new Listing
-			//{
-			//    Title = "Test " + listingCount.ToString(),
-			//    Description = "Description " + listingCount.ToString(),
-			//    BedCount = listingCount,
-			//    BathCount = listingCount,
-			//    Price = listingCount,
-			//    Date = 110224,
-			//    Picture = "image"
-			//});
-			//LoadItems();
+			listingCount++;
+			LoadItems();
+			StateHasChanged();
 		}
 
         private void RemoveItem()
         {
 
         }
-    }
+	}
 }
+
+
+
+//Listing l1 = new Listing
+//{
+//	Title = "Test1 " + listingCount.ToString(),
+//	Description = "Description " + listingCount.ToString(),
+//	BedCount = listingCount,
+//	BathCount = listingCount,
+//	Price = listingCount,
+//	Date = 110224,
+//	Picture = "image"
+//};
+
+//Listing l2 = new Listing
+//{
+//	Title = "Test2 " + listingCount.ToString(),
+//	Description = "Description " + listingCount.ToString(),
+//	BedCount = listingCount,
+//	BathCount = listingCount,
+//	Price = listingCount,
+//	Date = 110224,
+//	Picture = "image"
+//};
+
+//Listing l3 = new Listing
+//{
+//	Title = "Test3 " + listingCount.ToString(),
+//	Description = "Description " + listingCount.ToString(),
+//	BedCount = listingCount,
+//	BathCount = listingCount,
+//	Price = listingCount,
+//	Date = 110224,
+//	Picture = "image"
+//};
+
+//Listing l4 = new Listing
+//{
+//	Title = "Test4 " + listingCount.ToString(),
+//	Description = "Description " + listingCount.ToString(),
+//	BedCount = listingCount,
+//	BathCount = listingCount,
+//	Price = listingCount,
+//	Date = 110224,
+//	Picture = "image"
+//};
+
+//Listing l5 = new Listing
+//{
+//	Title = "Test5 " + listingCount.ToString(),
+//	Description = "Description " + listingCount.ToString(),
+//	BedCount = listingCount,
+//	BathCount = listingCount,
+//	Price = listingCount,
+//	Date = 110224,
+//	Picture = "image"
+//};
+
+//Listing l6 = new Listing
+//{
+//	Title = "Test6 " + listingCount.ToString(),
+//	Description = "Description " + listingCount.ToString(),
+//	BedCount = listingCount,
+//	BathCount = listingCount,
+//	Price = listingCount,
+//	Date = 110224,
+//	Picture = "image"
+//};
