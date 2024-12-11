375bdcb42d5055bf1d882f4e76692589608e4c34
Eby <133599375+Eberechi-N@users.noreply.github.com>
2024-12-10 19:08:50 -0600

    testing one way of have persistant data through diffeerent pages

---
+++ HMS/Components/Pages/BookingService.cs
@@ -0,0 +1,9 @@
+public class BookingService
+{
+    public string HotelName { get; set; }
+    public string RoomType { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+    public string Guests { get; set; }
+    public double TotalPrice { get; set; }
+}
